1e7258bf23f643301ec375ae6df064e4be81aee1
Bharat Goel <bharat.goel@harness.io>
2024-11-22 17:48:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,8 @@ variable "replicas" {
 # Use templatefile to load and render the YAML template
 
 resource "aws_api_gateway_rest_api" "rest_api" {
-
+  name        = "example-rest-api"
+  description = "This is an example REST API"
   value = templatefile("cust.yaml", {})
 
 }
